f779e2f4adbbd8f6b92c67545c8f5005d24e6725
MartinBrandstrup <44898614+MartinBrandstrup@users.noreply.github.com>
2020-04-16 15:12:31 +0200

    Update Settings.js

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 
-// URL for the deployed server (fetch data will add /api/info/user)
+// URL for the deployed server. All endpoints are to be added below.
 const URL = "https://cakehr.dk/2020CA3";
 
 // URL for the Local vagrant server
 //const URL = "http://localhost:8080/2020CA3";
 
-export default URL;
+
+/*
+Following URLs are modular and can be changed according to the requirements of the project.
+*/
+const userDataURL = "/api/info/user";
+const loginURL = "/api/login";
+
+export { URL, userDataURL, loginURL };
