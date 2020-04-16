c2031f6d9b07cd5a4b6424b8ecd65900e7f3b5b8
MartinBrandstrup <44898614+MartinBrandstrup@users.noreply.github.com>
2020-04-16 15:12:40 +0200

    Create ApiFacade.js

---
+++ src/ApiFacade.js
@@ -0,0 +1,76 @@
+import { URL, userDataURL, loginURL } from "./Settings/Settings"
+
+function handleHttpErrors(res) {
+  if (!res.ok) {
+    return Promise.reject({ status: res.status, fullError: res.json() });
+  }
+  return res.json();
+}
+
+function ApiFacade() {
+  const setToken = (token) => {
+    localStorage.setItem("jwtToken", token);
+  };
+  // places token in local storage (use dev tool Chrome ctrl + shift + i to confirm)
+  const getToken = () => {
+    return localStorage.getItem("jwtToken");
+  };
+
+  const loggedIn = () => {
+    const loggedIn = getToken() != null;
+    return loggedIn;
+  };
+
+  // removes token when logout button is clicked
+  const logout = () => {
+    localStorage.removeItem("jwtToken");
+  };
+
+  const login = (user, password) => {
+    const options = makeOptions("POST", true, {
+      username: user,
+      password: password,
+    });
+    return fetch(URL + loginURL, options)
+      .then(handleHttpErrors)
+      .then((res) => {
+        setToken(res.token);
+      });
+  };
+
+  const fetchData = () => {
+    const options = makeOptions("GET", true); // true adds the token
+    return fetch(URL + userDataURL, options).then(handleHttpErrors);
+  };
+
+  const makeOptions = (method, addToken, body) => {
+    var opts = {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    };
+
+    if (addToken && loggedIn()) {
+      opts.headers["x-access-token"] = getToken();
+    }
+    if (body) {
+      opts.body = JSON.stringify(body);
+    }
+    return opts;
+  };
+
+  return {
+    makeOptions,
+    setToken,
+    getToken,
+    loggedIn,
+    login,
+    logout,
+    fetchData,
+  };
+}
+
+const facade = ApiFacade();
+export default facade;
