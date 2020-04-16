1c3f4eeda657a1a3847b52a9e49497e4072f0163
MartinBrandstrup <44898614+MartinBrandstrup@users.noreply.github.com>
2020-04-16 15:11:53 +0200

    Added template for Jokes.js

---
+++ src/REST/Jokes.js
@@ -0,0 +1,9 @@
+import React from "react";
+import facade from "../ApiFacade";
+import { handleHttpErrors } from "../ApiFacade";
+import { URL, allJokesURL } from "../Settings/Settings";
+
+function GETJokes() {
+    const options = facade.makeOptions("GET", true);
+    return fetch(URL + allJokesURL, options).then(handleHttpErrors);
+};
